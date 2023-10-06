3734fa1c5fb1f9a79325921ff8f5d11bec1443b3
Arthur Luziano <luzianoarthur@gmail.com>
2023-10-06 11:44:53 -0300

    Update script.js

---
+++ aula12ex/ex014/js/script.js
@@ -0,0 +1,24 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+
+    var data = new Date()
+    var hora = data.getHours()
+
+    var header = document.getElementsByTagName('header')
+    
+    msg.innerHTML = `Agora são ${hora} horas!`
+
+    if (hora >= 5 && hora < 12){
+        img.src = 'fotos/fotomanha.png'
+        document.body.style.background = '#f5d3a1'
+
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'fotos/fototarde.png'
+        document.body.style.background = '#fcb941'
+
+    } else {
+        img.src = 'fotos/fotonoite.png'
+        document.body.style.background = '#213c4b'
+    }
+}
