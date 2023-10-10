b60090618f6ae29c96282b49a018973f59e8ff97
Arthur Luziano <luzianoarthur@gmail.com>
2023-10-10 16:59:42 -0300

    Update script.js

--- aula14ex/ex016/js/script.js
+++ aula14ex/ex016/js/script.js
@@ -3,6 +3,12 @@ function contador(){
     var Final = window.document.getElementById('final').value
     var Step = window.document.getElementById('step').value
     var Result = window.document.getElementById('result').value
+    var Total = window.document.getElementById('total')
 
-    
+    Result = Final - Initial
+
+    while (Initial <= Final){
+        Total.innerHTML = (Result)
+        Initial += Step
+    }
 }
