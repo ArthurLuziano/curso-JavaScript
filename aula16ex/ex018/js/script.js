3e673164488d5a6f28d32637573e827d45542868
Arthur Luziano <luzianoarthur@gmail.com>
2023-10-13 15:31:55 -0300

    Update script.js

--- aula16ex/ex018/js/script.js
+++ aula16ex/ex018/js/script.js
@@ -6,14 +6,11 @@ function getValue(){
     let num = Number(n.value)
     console.log(numbers)
 
-    if (num < 1 || num > 100){
-        alert('O número é invalido')
-    }
-    
     if (numbers.indexOf(num) != -1){
         alert('O número já existe')
-        console.log(numbers.indexOf(num))
-    } else{
+    } else if (num < 1 || num > 100){
+        alert('O número é invalido')
+    } else {
         let item = document.createElement('option')
         item.text = `Valor ${num} adicionado.`
         item.value = num
